889cb1e145c5f82dce1401352648d9e7827b481a
cristiaan3003 <cristiaan3003@gmail.com>
2019-07-07 19:46:15 -0300

    week 2 - leccion 3 - Arquitectura de aplicaciones reactivas

--- week1/angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ week1/angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
 import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { fromEvent, pipe } from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+import { map,filter,debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -11,6 +14,7 @@ export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
   minLongitud = 3;
+  searchResults: string[];
   constructor(fb: FormBuilder) { 
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
@@ -26,6 +30,20 @@ export class FormDestinoViajeComponent implements OnInit {
   }
 
   ngOnInit() {
+    let elemNombre = <HTMLInputElement> document.getElementById('nombre');
+    fromEvent(elemNombre,'input')// (obserable de eventos)listener de las teclas que presiona el usuario
+    .pipe(
+      map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+      filter(text => text.length > 2), //largo minimo del texto
+      debounceTime(200),// delay de tecleado
+      distinctUntilChanged(),// si hubo cambios en lo que llega del teclado
+      switchMap(() => ajax('/assets/datos.json')) //este texto a buscar se lo pasamos a un API de busqueda
+
+    ).subscribe(AjaxResponse => {
+      console.log(AjaxResponse)
+      console.log(AjaxResponse.response)
+      this.searchResults = AjaxResponse.response;
+    })
   }
   
   guardar(nombre: string, url: string):boolean{
