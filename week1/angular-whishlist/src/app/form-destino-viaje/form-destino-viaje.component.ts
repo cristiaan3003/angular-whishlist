44966f93fc57ca02b030850b336ab4e81c36ae49
cristiaan3003 <cristiaan3003@gmail.com>
2019-07-07 16:52:48 -0300

    week 2 - leccion 2 - validadores personalizados

--- week1/angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ week1/angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -10,10 +10,14 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
+  minLongitud = 3;
   constructor(fb: FormBuilder) { 
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
-      nombre: ['', Validators.required],
+      nombre: ['', Validators.compose([
+        Validators.required,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url: ['']
     });
     this.fg.valueChanges.subscribe((form: any)=>{
@@ -28,6 +32,23 @@ export class FormDestinoViajeComponent implements OnInit {
     let d = new DestinoViaje(nombre,url);
     this.onItemAdded.emit(d);
     return false;
+  }
+
+  nombreValidator(control : FormControl):{[s: string]: boolean}{
+    let l = control.value.toString().trim().length;
+    if(l>0 && l<5){
+      return {invalidNombre: true};
+    }
+    return null;
+  }
+  nombreValidatorParametrizable(minLong: number): ValidatorFn{
+    return(control: FormControl):{[s: string]:boolean}| null =>{
+      let l = control.value.toString().trim().length;
+      if(l>0 && l<minLong){
+        return {minLongNombre: true};
+      }
+      return null;
+    }
   }
 
 }
