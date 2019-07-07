31b1ba652f2b93afd618aa6e980acaa567e4318b
cristiaan3003 <cristiaan3003@gmail.com>
2019-07-07 10:43:39 -0300

    form-destino-viaje

--- week1/angular-whishlist/src/app/app.module.ts
+++ week1/angular-whishlist/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule,Routes} from '@angular/router'
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
 
 const routes: Routes =[
   {path: '',redirectTo: 'home',pathMatch: 'full'},
@@ -19,10 +22,13 @@ const routes: Routes =[
     AppComponent,
     DestinoViajeComponent,
     ListaDestinosComponent,
-    DestinoDetalleComponent
+    DestinoDetalleComponent,
+    FormDestinoViajeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     RouterModule.forRoot(routes)
   ],
