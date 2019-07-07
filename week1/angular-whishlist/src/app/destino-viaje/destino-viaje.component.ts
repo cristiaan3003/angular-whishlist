afd1d9d6884e85f7898f6e517fa95de6722d623a
cristiaan3003 <cristiaan3003@gmail.com>
2019-07-07 10:34:33 -0300

    week 2 - Lección 1: Componentes, Vistas, Rutas y Controladores

--- week1/angular-whishlist/src/app/destino-viaje/destino-viaje.component.ts
+++ week1/angular-whishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, EventEmitter, Output } from '@angular/core';
 import {DestinoViaje} from '../models/destino-viaje.model'
 
 @Component({
@@ -8,11 +8,18 @@ import {DestinoViaje} from '../models/destino-viaje.model'
 })
 export class DestinoViajeComponent implements OnInit {
   @Input() destino: DestinoViaje; //nombre puede ser pasado como parametro a las plantillas del componente
+  @Input('idx') position: number; //renombro la variable a nivel "template"-> osea en el template se llamara 'idx'
   @HostBinding('attr.class') cssClass='col-md-4';
+  @Output() clicked: EventEmitter<DestinoViaje>;
   constructor() {
+    this.clicked = new EventEmitter();
   }
 
   ngOnInit() {
   }
 
+  ir(){
+    this.clicked.emit(this.destino);
+    return false;
+  }
 }
